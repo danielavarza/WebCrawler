a4f910d63052fc58ef97a24b14ec1e4632972c8c
danielavarza <varza.daniela@yahoo.com>
2023-03-06 16:13:18 +0200

    Refactored sort method using lambdas

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/WordCounts.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/WordCounts.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 /**
  * Utility class that sorts the map of word counts.
@@ -28,16 +29,24 @@ final class WordCounts {
   static Map<String, Integer> sort(Map<String, Integer> wordCounts, int popularWordCount) {
 
     // TODO: Reimplement this method using only the Stream API and lambdas and/or method references.
+    // collect statement: https://stackoverflow.com/questions/29567575/sort-map-by-value-using-lambdas-and-streams
+    // using limit option to limit the stream size
+    // sort using the compare() method from WordCountComparator class.
 
-    PriorityQueue<Map.Entry<String, Integer>> sortedCounts =
-        new PriorityQueue<>(wordCounts.size(), new WordCountComparator());
-    sortedCounts.addAll(wordCounts.entrySet());
-    Map<String, Integer> topCounts = new LinkedHashMap<>();
-    for (int i = 0; i < Math.min(popularWordCount, wordCounts.size()); i++) {
-      Map.Entry<String, Integer> entry = sortedCounts.poll();
-      topCounts.put(entry.getKey(), entry.getValue());
-    }
-    return topCounts;
+    return wordCounts.entrySet().stream()
+            .sorted(new WordCountComparator())
+            .limit(Math.min(popularWordCount, wordCounts.size()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                    (x, y) -> x, LinkedHashMap::new));
+
+    // Tried with multiple comparing statements - Not Working
+//    return wordCounts.entrySet().stream()
+//            .sorted(Comparator.comparing(Map.Entry<String, Integer>::getValue)
+//                    .thenComparing(Map.Entry::getKey)
+//                    .thenComparing(Comparator.comparing(Object::toString)))
+//            .limit(Math.min(popularWordCount, wordCounts.size()))
+//            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+//                    (x, y) -> x, LinkedHashMap::new));
   }
 
   /**
