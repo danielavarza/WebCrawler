220ee0de38bf9d7904eef74f7033c60243e2def5
danielavarza <varza.daniela@yahoo.com>
2023-03-06 13:04:01 +0200

    Added implementation for Legacy Crawler

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -36,8 +36,24 @@ public final class WebCrawlerMain {
 
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+
+    // Write the crawl results to a JSON file (or System.out if the file name is empty)
+    if(!config.getResultPath().isEmpty()) {
+      resultWriter.write(Path.of(config.getResultPath()));
+    }
+    else {
+      // Convert System.out to OutputStreamWriter to be compatible
+      resultWriter.write(new java.io.OutputStreamWriter(System.out));
+    }
+
+    // Write the profile data to a text file (or System.out if the file name is empty)
+    if(!config.getProfileOutputPath().isEmpty()) {
+      profiler.writeData(Path.of(config.getProfileOutputPath()));
+    }
+    else {
+      // Convert System.out to OutputStreamWriter to be compatible
+      profiler.writeData(new java.io.OutputStreamWriter(System.out));
+    }
   }
 
   public static void main(String[] args) throws Exception {
